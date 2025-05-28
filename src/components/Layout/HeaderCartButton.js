d90006268dafaa98e8c8579535c8b4ac3c720ce6
Shikhar <shikharkatiyar558@gmail.com>
2025-05-28 22:51:57 +0530

    DIY- Implementing the cart Itemns page

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,26 @@
+import React, { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCartButton = (props) =>{
+
+    const cartCntxt = useContext(CartContext);
+
+    let quantity = 0;
+    cartCntxt.items.forEach(item =>{
+        quantity = quantity + Number(item.quantity)
+    })
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{quantity}</span>
         </button>
     )
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,28 @@
+import React, { useContext } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+import CartCntx from "../../../store/cart-context";
 
 const MealItemForm = props => {
+    const cartcntx = useContext(CartCntx);
+
+    const addItemToCart = (event) =>{
+        event.preventDefault();
+        //update the cartcntx.items
+        const quantity = document.getElementById('amount_' +props.id).value;
+        cartcntx.addItem({...props.item, quantity: quantity })
+
+    }
     return(
         <form className={classes.form}>
-            <Input label="Amount" input={{id:'amount',
+            <Input label="Amount" input={{id:'amount_' +props.id,
                 type:'number',
                 min:'1',
                 max:'5',
                 step:'1',
                 defaultValue: '1'
             }}/>
-            <button>+ Add</button>
+            <button onClick={addItemToCart}>+  Add</button>
         </form>
     )
 };
